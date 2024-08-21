aaf08acccff8adc04c1c89afab39617b9f54c1b7
Oscar Andersson <100211209+Ozzcarr@users.noreply.github.com>
2024-08-21 18:10:22 +0200

    Update install_vscode_extensions.sh

--- install_vscode_extensions.sh
+++ install_vscode_extensions.sh
@@ -2,28 +2,25 @@
 
 # List of VSCode extensions to install
 extensions=(
-    aaron-bond.better-comments
-    akamud.vscode-theme-onedark
     albert.tabout
     christian-kohler.path-intellisense
     donjayamanne.python-environment-manager
     esbenp.prettier-vscode
     github.copilot
     github.copilot-chat
-    hashicorp.terraform
-    ms-azure-devops.azure-pipelines
-    ms-azuretools.vscode-azureresourcegroups
+    mechatroner.rainbow-csv
     ms-python.black-formatter
     ms-python.debugpy
     ms-python.flake8
     ms-python.isort
     ms-python.python
     ms-python.vscode-pylance
-    ms-vscode.azure-account
-    pkief.material-icon-theme
-    sandcastle.vscode-open
-    usernamehw.errorlens
-    vscodevim.vim
+    ms-toolsai.jupyter
+    ms-toolsai.jupyter-keymap
+    ms-toolsai.jupyter-renderers
+    ms-toolsai.vscode-jupyter-cell-tags
+    ms-toolsai.vscode-jupyter-slideshow
+    shd101wyy.markdown-preview-enhanced
 )
 
 # Loop through the list and install each extension
